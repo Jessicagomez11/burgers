8a387c2b157c9f8bfaff8c10abf4876356112161
Jessica  Gomez <jessicagomez@Jessicas-MacBook-Air.local>
2019-06-01 23:06:30 -0700

    jawsdb_url

--- Solved/config/connection.js
+++ Solved/config/connection.js
@@ -1,7 +1,13 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+
+
+var connection;
+if (process.env.JAWSDB_url){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else{
+ mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -9,6 +15,7 @@ var connection = mysql.createConnection({
   database: "burgers_db",
   
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
